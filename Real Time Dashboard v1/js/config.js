c2d5e1b1ce23d55e3afd4789268128709649467a
Lakshay Kumar <lakshkumar846@gmail.com>
2021-09-30 17:09:41 +0530

    Initial commit

---
+++ Real Time Dashboard v1/js/config.js
@@ -0,0 +1,13 @@
+/* Configuration Variables
+    confg.username: Your Web Services Username. This can be found in the Admin Console
+    config.secret: Your Web Services Secret. This can also be found in the Admin Console
+    config.reportSuite: The report suite you want to call the data from
+    config.endpoint: The endpoint you will be hitting
+*/
+
+var config = {
+    username:     "lakshay.kumar@tadigital.com:TechAspect Solutions Inc Americas Partner Sandbox",
+    secret:       "cdf71720506df0f7c7e99a6553518def",
+    reportSuite:  "ageo1xxpnwlakshaytest",
+    endpoint:     "api.omniture.com"
+};
