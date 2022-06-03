0f1a087b8b70a8bf226b6fdcd03d3174120f6f25
Lakshay Kumar <lakshkumar846@gmail.com>
2022-06-03 13:55:55 +0530

    new commit

--- js/config.js
+++ js/config.js
@@ -5,17 +5,17 @@
     config.endpoint: The endpoint you will be hitting
 */
 
-// var config = {
-//     username:       "ritesh.gupta@tadigital.com:WRENCH GROUP LLC",
-//     secret:         "9023bdb5483f0853d4c41abec30a11bd",
-//     reportSuite:    "wrenchgroupcoolraymrplumberbriteboxprod",
-//     endpoint:       "api.omniture.com",
-// };
-
-
 var config = {
-    username: "lakshay.kumar@tadigital.com:TechAspect Solutions Inc Americas Partner Sandbox",
-    secret: "cdf71720506df0f7c7e99a6553518def",
-    reportSuite: "ageo1xxpnwlakshaytest",
-    endpoint: "api.omniture.com"
+    username:       "ritesh.gupta@tadigital.com:WRENCH GROUP LLC",
+    secret:         "9023bdb5483f0853d4c41abec30a11bd",
+    reportSuite:    "wrenchgroupcoolraymrplumberbriteboxprod",
+    endpoint:       "api.omniture.com",
 };
+
+
+// var config = {
+//     username: "lakshay.kumar@tadigital.com:TechAspect Solutions Inc Americas Partner Sandbox",
+//     secret: "cdf71720506df0f7c7e99a6553518def",
+//     reportSuite: "ageo1xxpnwlakshaytest",
+//     endpoint: "api.omniture.com"
+// };
